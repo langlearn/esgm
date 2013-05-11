a317bd5b163797ef2e5de0e06f1d753909decc44
shirokumacafe <wengyuchen@gmail.com>
2013-05-11 11:10:26 +0800

    恢复接口

---
+++ src/main/resources/com/bdt/service/WorkDiaryService.java
@@ -0,0 +1,21 @@
+package com.bdt.service;
+
+import com.bdt.bean.WorkDiary;
+import com.bdt.common.bean.Page;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lim
+ * Date: 13-5-11
+ * Time: 上午11:10
+ * To change this template use File | Settings | File Templates.
+ */
+public interface WorkDiaryService {
+    void add(WorkDiary model);
+
+    void modify(WorkDiary model);
+
+    void delete(String rids);
+
+    Page<WorkDiary> queryByPage(WorkDiary model, Integer start, Integer limit);
+}
