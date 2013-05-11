de1dd8586ad36c50e67c7c4d275cfe726dd62d59
shirokumacafe <wengyuchen@gmail.com>
2013-05-11 10:57:13 +0800

    恢复接口

--- src/main/resources/com/bdt/service/DataDictionaryService.java
+++ src/main/resources/com/bdt/service/DataDictionaryService.java
@@ -1,55 +1,21 @@
 package com.bdt.service;
 
 import com.bdt.bean.DataDictionary;
-import com.bdt.bean.DataDictionaryExample;
 import com.bdt.common.bean.Page;
-import com.bdt.common.util.MyStrUtil;
-import com.bdt.mapper.DataDictionaryMapper;
-import org.apache.commons.lang3.StringUtils;
-
-import javax.inject.Inject;
-import java.util.List;
 
 /**
- * 数据字典Service
- *
- * @author lim
+ * Created with IntelliJ IDEA.
+ * User: Lim
+ * Date: 13-5-11
+ * Time: 上午10:52
+ * To change this template use File | Settings | File Templates.
  */
-public class DataDictionaryService {
-    @Inject
-    private DataDictionaryMapper dataDictionaryMapper;
+public interface DataDictionaryService {
+    void add(DataDictionary model);
 
-    
-    public void add(DataDictionary model){
-        dataDictionaryMapper.insert(model);
-    }
+    void modify(DataDictionary model);
 
-    
-    public void modify(DataDictionary model){
-        dataDictionaryMapper.updateByPrimaryKeySelective(model);
-    }
+    void delete(String rids);
 
-    
-    public void delete(String rids){
-        List<Integer> ids= MyStrUtil.stringToListInteger(rids);
-        DataDictionaryExample example=new DataDictionaryExample();
-        DataDictionaryExample.Criteria criteria=example.createCriteria();
-        criteria.andDdIdIn(ids);
-        dataDictionaryMapper.deleteByExample(example);
-    }
-
-    
-    public Page<DataDictionary> queryByPage(DataDictionary model, Integer start, Integer limit){
-        Page<DataDictionary> page=new Page<DataDictionary>(start,limit);
-        DataDictionaryExample example=new DataDictionaryExample();
-        DataDictionaryExample.Criteria criteria=example.createCriteria();
-        if(StringUtils.isNotBlank(model.getDictionaryName())){
-            criteria.andDictionaryNameLike("%"+model.getDictionaryName()+"%");
-        }
-        int count= dataDictionaryMapper.countByExample(example);
-        List<DataDictionary> DataDictionarys= dataDictionaryMapper.selectByExample(example,page.createRowBounds());
-        page.setTotal(count);
-        page.setRoot(DataDictionarys);
-        return page;
-    }
+    Page<DataDictionary> queryByPage(DataDictionary model, Integer start, Integer limit);
 }
---
+++ src/main/resources/com/bdt/service/impl/DataDictionaryServiceImpl.java
@@ -0,0 +1,59 @@
+package com.bdt.service.impl;
+
+import com.bdt.bean.DataDictionary;
+import com.bdt.bean.DataDictionaryExample;
+import com.bdt.common.bean.Page;
+import com.bdt.common.util.MyStrUtil;
+import com.bdt.mapper.DataDictionaryMapper;
+import com.bdt.service.DataDictionaryService;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * 数据字典Service
+ *
+ * @author lim
+ */
+public class DataDictionaryServiceImpl implements DataDictionaryService {
+    @Inject
+    private DataDictionaryMapper dataDictionaryMapper;
+
+    @Override
+    public void add(DataDictionary model){
+        dataDictionaryMapper.insert(model);
+    }
+
+    
+    @Override
+    public void modify(DataDictionary model){
+        dataDictionaryMapper.updateByPrimaryKeySelective(model);
+    }
+
+    
+    @Override
+    public void delete(String rids){
+        List<Integer> ids= MyStrUtil.stringToListInteger(rids);
+        DataDictionaryExample example=new DataDictionaryExample();
+        DataDictionaryExample.Criteria criteria=example.createCriteria();
+        criteria.andDdIdIn(ids);
+        dataDictionaryMapper.deleteByExample(example);
+    }
+
+    
+    @Override
+    public Page<DataDictionary> queryByPage(DataDictionary model, Integer start, Integer limit){
+        Page<DataDictionary> page=new Page<DataDictionary>(start,limit);
+        DataDictionaryExample example=new DataDictionaryExample();
+        DataDictionaryExample.Criteria criteria=example.createCriteria();
+        if(StringUtils.isNotBlank(model.getDictionaryName())){
+            criteria.andDictionaryNameLike("%"+model.getDictionaryName()+"%");
+        }
+        int count= dataDictionaryMapper.countByExample(example);
+        List<DataDictionary> DataDictionarys= dataDictionaryMapper.selectByExample(example,page.createRowBounds());
+        page.setTotal(count);
+        page.setRoot(DataDictionarys);
+        return page;
+    }
+}
